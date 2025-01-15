925be69df10166fcb8a53ea270c7ee5230b58faa
gonudayo <kgw010119@gmail.com>
2025-01-15 09:36:16 +0900

    checked의 undefined 오류 수정

--- background.js
+++ background.js
@@ -1,40 +1,38 @@
 // 창 생성
 function createWindow() {
-    const now = new Date();
-    if (now.getDay() % 6 === 0) {
-        return;
-    }
-    const hh = now.getHours();
-    const mm = now.getMinutes();
-    const url = 'https://edu.ssafy.com/edu/main/index.do';
-    let checked = false;
+  const now = new Date();
+  if (now.getDay() % 6 === 0) {
+    return;
+  }
+  const hh = now.getHours();
+  const mm = now.getMinutes();
+  const url = "https://edu.ssafy.com/edu/main/index.do";
 
-    chrome.storage.sync.get(['checked'], function(result) {
-        checked = result.value;
-    });
-
+  chrome.storage.sync.get(["checked"], (result) => {
+    let checked = result.checked || false;
     if ((hh === 8 && 30 <= mm) || (hh === 18 && mm <= 30)) {
-        if (checked === false) {
-            chrome.windows.create({
-                url: url,
-                state: 'maximized',
-                focused: true
-            });
-            checked = true;
-        }
+      if (checked === false) {
+        chrome.windows.create({
+          url: url,
+          state: "maximized",
+          focused: true,
+        });
+        checked = true;
+      }
     } else {
-        checked = false;
+      checked = false;
     }
-    chrome.storage.sync.set({'checked': checked});
+    chrome.storage.sync.set({ checked: checked });
+  });
 }
 
 // 알람 감지시
 chrome.alarms.onAlarm.addListener(() => {
-    createWindow();
+  createWindow();
 });
 
 // 30초마다 알람
-chrome.alarms.create({periodInMinutes: 0.5});
+chrome.alarms.create({ periodInMinutes: 0.5 });
 
 // 최초 실행시
 createWindow();
