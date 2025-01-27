1af199129b5508b3ed12878a2e08955891bb2851
gonudayo <kgw010119@gmail.com>
2025-01-28 00:32:04 +0900

    임시: 2025년 공휴일 예외 처리

--- background.js
+++ background.js
@@ -1,7 +1,28 @@
+// (임시) 2025년 공휴일 리스트
+let cal = new Array(373);
+cal[28] = 1;
+cal[29] = 1;
+cal[30] = 1;
+cal[2 * 31 + 3] = 1;
+cal[4 * 31 + 5] = 1;
+cal[4 * 31 + 6] = 1;
+cal[5 * 31 + 6] = 1;
+cal[7 * 31 + 15] = 1;
+cal[9 * 31 + 3] = 1;
+cal[9 * 31 + 6] = 1;
+cal[9 * 31 + 7] = 1;
+cal[9 * 31 + 8] = 1;
+cal[9 * 31 + 9] = 1;
+cal[11 * 31 + 25] = 1;
+
 // 창 생성
 function createWindow() {
   const now = new Date();
   if (now.getDay() % 6 === 0) {
+    return;
+  }
+  // (임시) 2025년 공휴일 예외 처리
+  if (cal[now.getMonth() * 31 + now.getDate()] === 1) {
     return;
   }
   const hh = now.getHours();
